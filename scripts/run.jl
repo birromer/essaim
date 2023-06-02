47d931795b9a0198e65f71d8b78e1f9802812cc2
Bernardo Hummes <hummesbe@gmail.com>
2023-06-02 17:37:04 +0200

    added uuid and main file

--- scripts/run.jl
+++ scripts/run.jl
@@ -4,11 +4,9 @@ using DrWatson
 include(srcdir("Robot.jl"))
 include(srcdir("Draw.jl"))
 
-model = initialize_model(; seed = 1, δt = 0.001, N = 78, com_range = 25., vis_range = 50., history_size = 500, extent=(300.,300.), speed=1.0)
+model = initialize_model(; seed = 1, δt = 0.001, N = 15, com_range = 25., vis_range = 25., history_size = 500, extent=(100.,100.), speed=1.0)
 
 # fig, plot_dict, interaction_dict = make_figure(model, agent_laplacian_step!)
-
-run_simulator!(model, agent_laplacian_step!)
 
 #run_animation!(model, agent_laplacian_step!; n_steps=2500)
 
@@ -16,3 +14,4 @@ run_simulator!(model, agent_laplacian_step!)
 
 #make_animation!(model, agent_simple_step!; n_frames=1000, steps_per_frame=1)
 
+run_simulator!(model, agent_laplacian_step!)
---
+++ src/Essaim.jl
@@ -0,0 +1,14 @@
+
+using DrWatson
+@quickactivate "Essaim"
+
+include(srcdir("Robot.jl"))
+include(srcdir("Draw.jl"))
+
+function julia_main()::Cint
+    # do something based on ARGS?
+    model = initialize_model(; seed = 1, δt = 0.001, N = 15, com_range = 25., vis_range = 25., history_size = 500, extent=(100.,100.), speed=1.0)
+    run_simulator!(model, agent_laplacian_step!)
+
+    return 0 # if things finished successfully
+end
